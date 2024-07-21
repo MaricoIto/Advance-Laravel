5db0dd68c7e439402bee8cb7d8e31a13bc1499fc
Maricos <itomarico15@gmail.com>
2024-07-21 16:14:49 +0900

    2-6

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -2,90 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Author;
 use Illuminate\Http\Request;
-use App\Http\Requests\AuthorRequest;
+use App\Models\Author;
 
 class AuthorController extends Controller
 {
     public function index()
     {
-        $authors = Author::all();
+        $authors = Author::Paginate(4);
         return view('index', ['authors' => $authors]);
-    }
-
-    public function add()
-    {
-        return view('add');
-    }
-
-    public function create(AuthorRequest $request)
-    {
-        $form = $request->all();
-        Author::create($form);
-        return redirect('/');
-    }
-
-    public function edit(Request $request)
-    {
-        $author = Author::find($request->id);
-        return view('edit', ['form' => $author]);
-    }
-
-    public function update(AuthorRequest $request)
-    {
-        $form = $request->all();
-        unset($form['_token']);
-        Author::where('id', $request->id)->update($form);
-        return redirect('/');
-    }
-
-    public function delete(Request $request)
-    {
-        $author = Author::find($request->id);
-        return view('/delete', ['author' => $author]);
-    }
-
-    public function remove(Request $request)
-    {
-        //dd($request->all()); // 追加
-        Author::find($request->id)->delete();
-        return redirect('/');
-    }
-
-    public function find()
-    {
-        return view('find', ['input' => '']);
-    }
-
-    public function search(Request $request)
-    {
-        $item = Author::where('name', 'LIKE', "%{$request->input}%")->first();
-        $param = [
-            'input' => $request->input,
-            'item' => $item
-        ];
-        return view('find', $param);
-    }
-
-    public function bind(Author $author)
-    {
-        $data = [
-            'item' => $author,
-        ];
-        return view('author.binds', $data);
-    }
-
-    public function verror()
-    {
-        return view('verror');
-    }
-
-    public function relate(Request $request) //追記
-    {
-        $hasItems = Author::has('book')->get();
-        $noItems = Author::doesntHave('book')->get();
-        $param = ['hasItems' => $hasItems, 'noItems' => $noItems];
-        return view('author.index',$param);
     }
 }
